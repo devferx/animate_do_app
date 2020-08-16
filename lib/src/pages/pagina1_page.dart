40f51e6bcd749dd5747ab8d32cdda92edd52d882
devferx <ferkki@hotmail.com>
2020-08-16 16:56:42 -0400

    diseño de pagina 1 completada y dependencia de font awesome agregada

--- lib/src/pages/pagina1_page.dart
+++ lib/src/pages/pagina1_page.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Pagina1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Animate_do'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: FaIcon(FontAwesomeIcons.twitter),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: FaIcon(Icons.navigate_next),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: FaIcon(FontAwesomeIcons.play),
+      ),
       body: Center(
-        child: Text('Hola mundo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.new_releases,
+              color: Colors.blue,
+              size: 40,
+            ),
+            Text(
+              'Titulo',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            Text(
+              'Soy un texto pequeño',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Container(
+              width: 220,
+              height: 2,
+              color: Colors.blue,
+            )
+          ],
+        ),
       ),
     );
   }
