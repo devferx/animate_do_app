abfae97f9b6da8ad83fb152165faba32072ae56c
devferx <ferkki@hotmail.com>
2020-08-16 17:07:30 -0400

    animaciones en pagina1 implementadas

--- lib/src/pages/pagina1_page.dart
+++ lib/src/pages/pagina1_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Pagina1Page extends StatelessWidget {
@@ -6,49 +8,76 @@ class Pagina1Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animate_do'),
+        title: FadeIn(
+          duration: Duration(milliseconds: 500),
+          child: Text('Animate_do'),
+        ),
         actions: [
           IconButton(
             onPressed: () {},
             icon: FaIcon(FontAwesomeIcons.twitter),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: FaIcon(Icons.navigate_next),
+          SlideInLeft(
+            from: 100,
+            child: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (BuildContext context) => Pagina1Page(),
+                  ),
+                );
+              },
+              icon: FaIcon(Icons.navigate_next),
+            ),
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: FaIcon(FontAwesomeIcons.play),
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: FaIcon(FontAwesomeIcons.play),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              Icons.new_releases,
-              color: Colors.blue,
-              size: 40,
-            ),
-            Text(
-              'Titulo',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w200,
+            ElasticIn(
+              delay: Duration(milliseconds: 1100),
+              child: Icon(
+                Icons.new_releases,
+                color: Colors.blue,
+                size: 40,
               ),
             ),
-            Text(
-              'Soy un texto pequeño',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
+            FadeInDown(
+              delay: Duration(milliseconds: 200),
+              child: Text(
+                'Titulo',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w200,
+                ),
               ),
             ),
-            Container(
-              width: 220,
-              height: 2,
-              color: Colors.blue,
+            FadeInDown(
+              delay: Duration(milliseconds: 800),
+              child: Text(
+                'Soy un texto pequeño',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            FadeInLeft(
+              delay: Duration(milliseconds: 1100),
+              child: Container(
+                width: 220,
+                height: 2,
+                color: Colors.blue,
+              ),
             )
           ],
         ),
